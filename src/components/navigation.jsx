4a64d72a7c830e12efb40b4281ef1a15326ec9c3
Dennis Ukonu <ukonudennis64@gmail.com>
2022-09-16 16:39:58 +0100

    --revamp contact page

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -45,11 +45,11 @@ export const Navigation = (props) => {
                 Gallery
               </a>
             </li> */}
-            <li>
+            {/* <li>
               <a href='#testimonials' className='page-scroll'>
                 Testimonials
               </a>
-            </li>
+            </li> */}
             {/* <li>
               <a href='#team' className='page-scroll'>
                 Team
